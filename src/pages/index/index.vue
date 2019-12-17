0b69f0d79fc735e7cace0780420b421dd60da48b
houks <@houks>
2019-12-17 19:19:33 +0800

    save

---
+++ src/pages/index/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>mpvue</div>
+</template>
+
+<script>
+import card from '@/components/card'
+
+export default {
+  data () {
+    return {}
+  },
+
+  components: {
+    card
+  },
+
+  methods: {},
+
+  created () {
+    // let app = getApp()
+  }
+}
+</script>
+
+<style scoped>
+</style>
